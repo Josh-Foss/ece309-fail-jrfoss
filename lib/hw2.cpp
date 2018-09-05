70bb19b5f9da282cf4cea99739d82e30e547ee6c
Josh-Foss <42939702+Josh-Foss@users.noreply.github.com>
2018-09-04 22:09:05 -0400

    Update hw2.cpp

--- lib/hw2.cpp
+++ lib/hw2.cpp
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 
 class Item {
@@ -10,122 +11,95 @@ class Item {
 	}
 };
 
-class List {
-	public:
 
-// ListNode represents each 
-// node of the list
-	class ListNode {
-		
-		private:
+class ListNode {
+	private:
 		Item item; // data in the list
 		ListNode *next;	
-		
-		public:
+	public:
 		ListNode(Item a) 
-		{ 
-			item = a; 
-			next=NULL; // automatically serves as a list tail
-		}
-		
-		ListNode* getNext() //returns the pointer for the next node.
-		{ 
-			return next; 
-		}
-		
-		void setNext(ListNode *n) //sets pointer to next node. Helpful when inserting or deleteing items.
-		{ 
-			next = n; 
-		}
-		
-		Item getItem() //retrieves the item in a certain node
-		{ 
-			return item; 
-		}
-	};
-	
-	// add head and tail pointer
-	ListNode *head;
-	ListNode *tail;
-	
-	void push_back(char *a) 
-	{
-		ListNode *node = new ListNode(a);
-		if (head==NULL) { 
-			// list is empty, so set head and 
-			// tail to be node
-			head = node;
-			tail = node;
-		} 
-		else {
-			// put new node at end of list
-			tail->setNext(node);
-			tail = node;
-		}
-	}
-
-
-	Item get(int n)
-	{
-		int i;
-		Item ans;
-		for (i=0; i < n; i++) {
-			node = node->getNext();
-			if (node == NULL) {
-				return NULL;
+		{ item = a; next=NULL; }
+		ListNode* getNext() { return next; }
+		void setNext(ListNode *n) { next = n; }
+		Item getItem() { return item; }
+		void push_back(Item a) 
+		{
+			ListNode *node = new ListNode(a);
+			if (head==NULL) { 
+				// list is empty, so set head and 
+				// tail to be node
+				head = node;
+				tail = node;
+			} 
+			else {
+				// put new node at end of list
+				tail->setNext(node);
+				tail = node;
 			}
 		}
-		ans = node->getItem();
-		return ans;
-	}
 
-	int length()
-	{
-		if (head==NULL) {
-			return 0;
-		}
-		int i = 1;
-		while (next != NULL) {
-			i = i + 1;
-			next = node->next;
-		}
-		return i;
-	}
 
-	Item remove_front() 
-	{
-		if (!empty()) // if list is not empty
+		Item get(int n)
 		{
-		copy = head->getItem(); // return copy
-		ListNode *tmp = head->getNext();
-		delete head; // delete the node
-		head = tmp;  // update the head
-		if (tmp==NULL) // removed last element 
-			tail = NULL;
-		return true;
+			int i;
+			Item ans;
+			for (i=0; i < n; i++) {
+				node = node->getNext();
+				if (node == NULL) {
+					return NULL;
+				}
+			}
+			ans = node->getItem();
+			return ans;
 		}
-		return copy; // nothing in list
-	}
 
-	
-	List () {
-		head = NULL;
-		tail = NULL;
-	};
-	//a destructor
-	~List();
-	
+		int length()
+		{
+			if (head==NULL) {
+				return 0;
+			}
+			int i = 1;
+			while (next != NULL) {
+				i = i + 1;
+				next = node->next;
+			}
+			return i;
+		}
 
-	
-	
+		Item remove_front() 
+		{
+			if (!empty()) // if list is not empty
+			{
+			copy = head->getItem(); // return copy
+			ListNode *tmp = head->getNext();
+			delete head; // delete the node
+			head = tmp;  // update the head
+			if (tmp==NULL) // removed last element 
+				tail = NULL;
+			return true;
+			}
+			return copy; // nothing in list
+		}
 
-  
-	public:
-	List();
-	void append(Item a);    
-	bool remove(Item &a);    
-	bool empty(); 
+		//a destructor
+		~List()
+		{
+			// Free all of the ListNodes in the list
+			Item t;
+			while(!empty()){ // while not empty
+				remove(t);	// remove the next node
+			}	 
+		}
+
+		//a constructor
+		List() 
+		{
+			// there’s nothing in the list
+			head = NULL;
+			tail = NULL;
+		}
 };
+
 
 void List::append(Item a)
 {
@@ -164,24 +138,14 @@ bool List::empty()
 }
 
 
-int main()
-{
-	List a;
-
-	a.append(Item("Fraser"));
-	a.append(Item("Lucas"));
-	a.append(Item("Silva"));
-	a.append(Item("Mkhi"));
-	a.append(Item("Townsend"));
-	a.push_back(Item("Sessegnon"));
-
-	while(!a.empty())
-    {
-		Item copy;
-		a.remove(copy);
-
-		printf("Next thing removed: %s\n",copy.str);
-    }
-
-	return 0;
-}
+class List {
+	private:
+		// add head and tail pointer
+		ListNode *head;
+		ListNode *tail;
+	public:
+		List();
+		void append(Item a);    
+		bool remove(Item &a);    
+		bool empty(); 
+};
